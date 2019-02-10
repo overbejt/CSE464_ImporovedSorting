295c4b04b9d2a0f75c8c592023fe32898f8516ac
Josh Overbeck <overbejt@miamioh.edu>
2019-02-10 11:46:20 -0500

    ready to push first commit

---
+++ src/Insertion.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Josh Overbeck Professor: Vaskar Raychoudhurry Class: CSE
+ * 464 A Description: This program is for the assignment 'Improved
+ * Sorting'. The goal is to come up with an algorithm that improves on
+ * the merge sort. This class file will create the object necessary to
+ * perform an insertion sort.
+ * 
+ * @author joshoverbeck
+ */
+public class Insertion {
+	// Int
+
+} // End of the 'Insertion' class
---
+++ src/MergeSort.java
@@ -0,0 +1,17 @@
+/**
+ * Author: Josh Overbeck Professor: Vaskar Raychoudhurry Class: CSE
+ * 464 A Description: This program is for the assignment 'Improved
+ * Sorting'. The goal is to come up with an algorithm that improves on
+ * the merge sort. This class file will create the object necessary to
+ * perform a merge sort.
+ * 
+ * @author joshoverbeck
+ */
+public class MergeSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	} // End of the 'Main' method
+
+} // End of the 'MergeSort' class
