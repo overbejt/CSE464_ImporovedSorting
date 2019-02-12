0e61ab9389958f47055141106ba9c82b3454a735
Josh Overbeck <overbejt@miamioh.edu>
2019-02-11 22:28:25 -0500

    Wrote the merge Sort class.  Time to test

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -9,9 +9,69 @@
  */
 public class MergeSort {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// Instance Array
+	int[] arr;
 
-	} // End of the 'Main' method
+	// Constructor
+	public MergeSort() {
+
+	} // End of the 'Constructor'
+
+	public int[] sort(int[] input) {
+
+		// this.arr = input;
+
+		// Split
+		if (input.length > 2) {
+			int leftLength = input.length / 2;
+			int rightLength = input.length - leftLength;
+
+			int[] left = split(0, leftLength);
+			int[] right = split(input.length - leftLength,
+					input.length);
+
+			sort(left);
+			sort(right);
+		}
+		return input;
+	} // End of the 'sort' method
+
+	private int[] split(int start, int end) {
+		int[] result = new int[end];
+		for (int i = start; i < end; i++) {
+			result[i] = arr[i];
+		}
+		return result;
+	} // End of the 'split' method
+
+	private int[] merge(int[] left, int[] right) {
+		int[] merged = new int[left.length + right.length];
+		int leftIndex = 0;
+		int rightIndex = 0;
+		for (int i = 0; i < left.length + right.length; i++) {
+			if (left[leftIndex] < right[rightIndex]) {
+				merged[i] = left[leftIndex];
+				leftIndex++;
+			} else if (left[leftIndex] >= right[rightIndex]) {
+				merged[i] = right[rightIndex];
+				rightIndex++;
+			}
+		}
+		return merged;
+	} // End of the 'merge' method
+
+	/**
+	 * Method that will check if a swap is needed.
+	 * 
+	 * @param left
+	 *            - The left side value.
+	 * @param right
+	 *            - The right side value.
+	 * @return - returns true if the value on the right needs to be
+	 *         swapped to be before the left value.
+	 */
+	private boolean needSwap(int left, int right) {
+		return (left >= right);
+	} // End of the 'needSwap' method
 
 } // End of the 'MergeSort' class
--- src/Test.java
+++ src/Test.java
@@ -23,7 +23,10 @@ public class Test {
 				5, 5, 5, 4, 3, 2, 1 };
 
 		Insertion incert = new Insertion();
+		MergeSort merge = new MergeSort();
 
+		System.out.println(
+				"//////////////// Insertion Sort \\\\\\\\\\\\\\\\\\\\n");
 		System.out.println("Before A: ");
 		printArr(a);
 		a = incert.sort(a);
@@ -72,6 +75,59 @@ public class Test {
 		System.out.println("Before I: ");
 		printArr(i);
 		i = incert.sort(i);
+		System.out.println("After I: ");
+		printArr(i);
+
+		System.out.println(
+				"//////////////// Merge Sort \\\\\\\\\\\\\\\\\\\\n");
+		System.out.println("Before A: ");
+		printArr(a);
+		a = merge.sort(a);
+		System.out.println("After A: ");
+		printArr(a);
+		System.out.println("Before B: ");
+		printArr(b);
+		b = merge.sort(b);
+		System.out.println("After B: ");
+		printArr(b);
+		System.out.println("Before C: ");
+		printArr(c);
+		c = merge.sort(c);
+		System.out.println("After C: ");
+		printArr(c);
+		System.out.println("Before D: ");
+		printArr(d);
+		d = merge.sort(d);
+		System.out.println("After D: ");
+		printArr(d);
+
+		System.out.println("Before E: ");
+		printArr(e);
+		e = merge.sort(e);
+		System.out.println("After E: ");
+		printArr(e);
+
+		System.out.println("Before F: ");
+		printArr(f);
+		f = merge.sort(f);
+		System.out.println("After F: ");
+		printArr(f);
+
+		System.out.println("Before G: ");
+		printArr(g);
+		g = merge.sort(g);
+		System.out.println("After G: ");
+		printArr(g);
+
+		System.out.println("Before H: ");
+		printArr(h);
+		h = merge.sort(h);
+		System.out.println("After H: ");
+		printArr(h);
+
+		System.out.println("Before I: ");
+		printArr(i);
+		i = merge.sort(i);
 		System.out.println("After I: ");
 		printArr(i);
 	}
