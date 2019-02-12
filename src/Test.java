3b374cb3069cbbea270bd9017196b9a95f82d829
Josh Overbeck <overbejt@miamioh.edu>
2019-02-11 21:56:33 -0500

    Wrote tests for duplicates.  Insertion sort is GTG

--- src/Test.java
+++ src/Test.java
@@ -17,43 +17,63 @@ public class Test {
 		int[] g = {
 				1, 2, 3, 4, 5, 5 };
 
+		int[] h = {
+				5, 5, 4, 3, 2, 1 };
+		int[] i = {
+				5, 5, 5, 4, 3, 2, 1 };
+
 		Insertion incert = new Insertion();
 
-		System.out.println("Before: ");
+		System.out.println("Before A: ");
 		printArr(a);
 		a = incert.sort(a);
-		System.out.println("After: ");
+		System.out.println("After A: ");
 		printArr(a);
-		System.out.println("Before: ");
+		System.out.println("Before B: ");
 		printArr(b);
 		b = incert.sort(b);
-		System.out.println("After: ");
+		System.out.println("After B: ");
 		printArr(b);
-		System.out.println("Before: ");
+		System.out.println("Before C: ");
 		printArr(c);
 		c = incert.sort(c);
-		System.out.println("After: ");
+		System.out.println("After C: ");
 		printArr(c);
-		System.out.println("Before: ");
+		System.out.println("Before D: ");
 		printArr(d);
 		d = incert.sort(d);
-		System.out.println("After: ");
+		System.out.println("After D: ");
 		printArr(d);
 
+		System.out.println("Before E: ");
 		printArr(e);
-		d = incert.sort(e);
-		System.out.println("After: ");
+		e = incert.sort(e);
+		System.out.println("After E: ");
 		printArr(e);
 
+		System.out.println("Before F: ");
 		printArr(f);
-		d = incert.sort(f);
-		System.out.println("After: ");
+		f = incert.sort(f);
+		System.out.println("After F: ");
 		printArr(f);
 
+		System.out.println("Before G: ");
 		printArr(g);
-		d = incert.sort(g);
-		System.out.println("After: ");
+		g = incert.sort(g);
+		System.out.println("After G: ");
 		printArr(g);
+
+		System.out.println("Before H: ");
+		printArr(h);
+		h = incert.sort(h);
+		System.out.println("After H: ");
+		printArr(h);
+
+		System.out.println("Before I: ");
+		printArr(i);
+		i = incert.sort(i);
+		System.out.println("After I: ");
+		printArr(i);
 	}
 
 	public static void printArr(int[] in) {
