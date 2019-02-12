01623342722a6661a4754cc75c20581537720920
Josh Overbeck <overbejt@miamioh.edu>
2019-02-11 21:40:17 -0500

    Annotated the file provided by Dr. Vaskar

--- src/ImprovedSort.java
+++ src/ImprovedSort.java
@@ -1,35 +1,40 @@
 public class ImprovedSort {
 
-    private static void insertionSort(int[] arr) {
-        // Implement insertion sort, might need to modify parameters
-    }
+	private static void insertionSort(int[] arr) {
+		// Implement insertion sort, might need to modify parameters
+	} // End of the 'insertionSort' method
 
-    private static void merge(int[] arr) {
-        // Implement merge part here, might need to modify parameters
-    }
+	private static void merge(int[] arr) {
+		// Implement merge part here, might need to modify parameters
+	} // End of the 'merge' sort method
 
-    private static void impSort(int[] arr) {
-        // Implement improved sort
-        // should call insertionSort() and merge() at some point in this method
-    }
+	private static void impSort(int[] arr) {
 
-    private static void printArr(int[] arr) {
-        for (int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
+	} // End of the 'impSort' method
 
-    // Driver code -------- no need to touch
-    public static void main(String[] args) {
-        int[] testArr = new int[320];
-        for (int i = 0; i < testArr.length; i++)
-            testArr[i] = (int)(Math.random()*1000);
-        System.out.println("Before Sorting:");
-        printArr(testArr);
+	/**
+	 * This is a method that was provided by Dr. Vaskar.
+	 * 
+	 * @param arr
+	 *            - The array that is to be printed out.
+	 */
+	private static void printArr(int[] arr) {
+		for (int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	} // End of the 'printArr' method
 
-        impSort(testArr);
+	// Driver code -------- no need to touch
+	public static void main(String[] args) {
+		int[] testArr = new int[320];
+		for (int i = 0; i < testArr.length; i++)
+			testArr[i] = (int) (Math.random() * 1000);
+		System.out.println("Before Sorting:");
+		printArr(testArr);
 
-        System.out.println("After sorting:");
-        printArr(testArr);
-    }
-}
+		impSort(testArr);
+
+		System.out.println("After sorting:");
+		printArr(testArr);
+	} // End of the 'main' method
+} // End of the 'ImprovedSort' class
