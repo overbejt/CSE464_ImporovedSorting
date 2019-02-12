7b3f0e32963cf4b922e30d899b9f2555b63cee96
Josh Overbeck <overbejt@miamioh.edu>
2019-02-11 23:57:11 -0500

    fml

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -17,21 +17,31 @@ public class MergeSort {
 
 	} // End of the 'Constructor'
 
-	// public static int[] mergeSort(int[] original, int size) {
-	// if (size == 2) {
-	// // if (needSwap(original[0], original[1])) {
-	// // swap(original[0], original[1]);
-	// // }
-	// }
-	// if (size < 2) {
-	// // do nothing
-	// } else {
-	// int center = size / 2;
-	// int[] left = new int[center];
-	// int[] right = new int[size - center];
-	// }
-	//
-	// } // End of the 'mergeSort' method
+	public int[] mergeSort(int[] original) {
+
+		int size = original.length;
+		if (size == 1) {
+			return original;
+		} else if (size == 2) {
+			if (needSwap(original[0], original[1])) {
+				swap(original, original[0]);
+			}
+			return original;
+		} else {
+			int middle = size / 2;
+			int[] left = new int[middle];
+			int[] right = new int[size - middle];
+
+			for (int l = 0; l < middle; l++) {
+				left[l] = original[l];
+			}
+
+			for (int r = middle; r < size; r++) {
+
+			}
+		}
+
+	} // End of the 'mergeSort' method
 
 	/**
 	 * Method that will check if a swap is needed.
@@ -47,6 +57,24 @@ public class MergeSort {
 		return (left >= right);
 	} // End of the 'needSwap' method
 
-	/////////////////////////////////////////////////////
+	/**
+	 * This is the method that will handle the swapping of values. It
+	 * takes the current index, and rotates it to the left.
+	 * 
+	 * @param original
+	 *            - The array that is being worked on.
+	 * @param index
+	 *            - The current index of where to swap at.
+	 * @return - Sends the array back
+	 */
+	private int[] swap(int[] original, int index) {
+
+		if (index >= 1) {
+			int tmp = original[index - 1];
+			original[index - 1] = original[index];
+			original[index] = tmp;
+		}
+		return original;
+	} // End of the 'swap' method
 
 } // End of the 'MergeSort' class
