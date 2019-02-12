d57b8c6e2e171661d2145899622d646baf70b7b1
Josh Overbeck <overbejt@miamioh.edu>
2019-02-12 01:21:52 -0500

    it works perfectly.  Awesome.  Hope it's not too late

--- src/ImprovedSort.java
+++ src/ImprovedSort.java
@@ -13,17 +13,111 @@
  */
 public class ImprovedSort {
 
+	/**
+	 * External invocation method to perform insertion sort.
+	 * 
+	 * @param arr
+	 *            - Takes in an integer array.
+	 */
 	private static void insertionSort(int[] arr) {
-		// Implement insertion sort, might need to modify parameters
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (j >= 1) {
+					if (needSwap(arr[j - 1], arr[j])) {
+						swap(arr, j);
+					}
+				}
+			}
+		}
 	} // End of the 'insertionSort' method
 
 	private static void merge(int[] arr) {
-		// Implement merge part here, might need to modify parameters
+		int size = arr.length;
+		if (size == 1) {
+			return;
+		} else if (size == 2) {
+			if (needSwap(arr[0], arr[1])) {
+				swap(arr, 1);
+			}
+			return;
+		} else {
+			int middle = size / 2;
+			int[] left = new int[middle];
+			int[] right = new int[size - middle];
+
+			for (int l = 0; l < middle; l++) {
+				left[l] = arr[l];
+			}
+
+			for (int r = 0; r + middle < size; r++) {
+				right[r] = arr[r + middle];
+			}
+
+			merge(left);
+			merge(right);
+
+			assemble(arr, left, right);
+
+		}
 	} // End of the 'merge' sort method
 
+	/**
+	 * This is the method that will improve on the sorting. When there
+	 * are more than 32 blocks, then it will use merge sort. Otherwise
+	 * it will use insertion sort.
+	 * 
+	 * @param arr
+	 *            - The blocks to be sorted.
+	 */
 	private static void impSort(int[] arr) {
-
+		if (arr.length < 32) {
+			insertionSort(arr);
+		} else {
+			merge(arr);
+		}
 	} // End of the 'impSort' method
+
+	/**
+	 * This is the method that will merge it all back together.
+	 * 
+	 * @param original
+	 *            - The original array.
+	 * @param left
+	 *            - The left array.
+	 * @param right
+	 *            - The right array.
+	 */
+	private static void assemble(int[] original, int[] left,
+			int[] right) {
+
+		int lIndex = 0;
+		int rIndex = 0;
+
+		for (int i = 0; i < original.length; i++) {
+			if ((lIndex < left.length) && (rIndex < right.length)) {
+				if (needSwap(left[lIndex], right[rIndex])) {
+					original[i] = right[rIndex];
+					rIndex++;
+				} else {
+					original[i] = left[lIndex];
+					lIndex++;
+				}
+			}
+
+			// If left is empty
+			else if (lIndex == left.length) {
+				original[i] = right[rIndex];
+				rIndex++;
+			}
+
+			// If right is empty
+			else if (rIndex == right.length) {
+				original[i] = left[lIndex];
+				lIndex++;
+			}
+		}
+
+	} // End of the 'assemble' method
 
 	/**
 	 * Method that will check if a swap is needed.
@@ -35,7 +129,7 @@ public class ImprovedSort {
 	 * @return - returns true if the value on the right needs to be
 	 *         swapped to be before the left value.
 	 */
-	private boolean needSwap(int left, int right) {
+	private static boolean needSwap(int left, int right) {
 		return (left >= right);
 	} // End of the 'needSwap' method
 
@@ -49,7 +143,7 @@ public class ImprovedSort {
 	 *            - The current index of where to swap at.
 	 * @return - Sends the array back
 	 */
-	private int[] swap(int[] original, int index) {
+	private static int[] swap(int[] original, int index) {
 
 		if (index >= 1) {
 			int tmp = original[index - 1];
